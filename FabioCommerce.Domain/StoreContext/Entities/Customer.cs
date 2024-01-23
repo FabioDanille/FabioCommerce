6800293e69831f205542bd425a9a381114f6180b
Fabio Correia <fabiodanille@hotmail.com>
2024-01-22 21:58:22 -0300

    Adicionando Collections

--- FabioCommerce.Domain/StoreContext/Entities/Customer.cs
+++ FabioCommerce.Domain/StoreContext/Entities/Customer.cs
@@ -4,6 +4,7 @@ namespace FabioCommerce.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
 
         public Customer(
             Name name,
@@ -15,18 +16,23 @@ namespace FabioCommerce.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
 
         public override string ToString()
         {
             return Name.ToString();
+        }
+
+        public void AddAddres(Address address)
+        {
+            _addresses.Add(address);
         }
     }
 }
